212bd22964184c7a1944d8e5664fcf3eb58fab4f
Nico Mogicato <n.mogicato@gmail.com>
2023-01-31 19:20:12 +0100

    first commit

---
+++ build/src/CMakeFiles/pbrbasic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/pbrbasic"
+  "../bin/pbrbasic.pdb"
+  "CMakeFiles/pbrbasic.dir/pbrbasic/pbrbasic.cpp.o"
+  "CMakeFiles/pbrbasic.dir/pbrbasic/pbrbasic.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pbrbasic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/pbrbasic.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for pbrbasic.
+# This may be replaced when dependencies are built.
